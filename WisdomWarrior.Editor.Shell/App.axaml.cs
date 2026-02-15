c9e3b7c7324995ffacf0a0be35a5eaee38dce6fd
Ed Brown <eduard.brown@edbwn.com>
2026-02-15 15:14:56 +0000

    Redone the editor to be more simple. and organised the layout

--- WisdomWarrior.Editor.Shell/App.axaml.cs
+++ WisdomWarrior.Editor.Shell/App.axaml.cs
@@ -1,19 +1,11 @@
-using System;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
-using Dock.Model.Core;
-using Microsoft.Extensions.DependencyInjection;
-using WisdomWarrior.Editor.Core;
-using WisdomWarrior.Editor.Shell.Services;
-using WisdomWarrior.Editor.Shell.ViewModels;
 
 namespace WisdomWarrior.Editor.Shell;
 
 public partial class App : Application
 {
-    public IServiceProvider? Services { get; private set; }
-
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -21,34 +13,9 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
-        var serviceCollection = new ServiceCollection();
-
-        // 1. Create Layout
-        var factory = new ShellLayoutFactory();
-        var layout = factory.CreateLayout();
-        factory.InitLayout(layout);
-
-        // 2. Register
-        serviceCollection.AddSingleton<IFactory>(factory);
-        serviceCollection.AddSingleton<IDock>(layout);
-        serviceCollection.AddSingleton<IShell, ShellService>();
-        serviceCollection.AddSingleton<MainWindowViewModel>();
-
-        var loader = new ModuleLoader();
-        var pluginPath = System.IO.Path.Combine(AppContext.BaseDirectory, "Plugins");
-        loader.LoadModules(pluginPath, serviceCollection);
-
-        Services = serviceCollection.BuildServiceProvider();
-
-        loader.InitializeModules(Services);
-
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            var mainVm = Services.GetRequiredService<MainWindowViewModel>();
-            desktop.MainWindow = new Views.MainWindow
-            {
-                DataContext = mainVm
-            };
+            desktop.MainWindow = new MainWindow();
         }
 
         base.OnFrameworkInitializationCompleted();
---
+++ WisdomWarrior.Editor.Shell/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+using SukiUI.Controls;
+
+namespace WisdomWarrior.Editor.Shell;
+
+public partial class MainWindow : SukiWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
